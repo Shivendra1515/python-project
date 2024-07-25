166ed11ea9a5a2358f34ba29f1a9911a622ddcd5
Shivendra1515 <tdex1515@gmail.com>
2024-07-25 12:37:22 +0530

    Delete Robo speaker/image-resizer/main.py

--- Robo speaker/image-resizer/main.py
+++
@@ -1,7 +0,0 @@
-import cv2
-# by using cv2 and its methods you can upskill to many program and can make several things
-image = cv2.imread("tech.png", cv2.IMREAD_UNCHANGED)
-cv2.imshow("title", image)
-
-cv2.waitKey(0)
-
